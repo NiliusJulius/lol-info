5ff231daf79154b2380b87321a2bd3388c36aeab
nhouten <nielsvhouten@gmail.com>
2023-05-29 13:33:58 +0200

    Change the summoner search button from onclick to onsubmit

--- src/main/resources/static/js/find-summoner.js
+++ src/main/resources/static/js/find-summoner.js
@@ -1,8 +1,9 @@
-function findSummoner() {
+document.getElementById("find-summoner-form").addEventListener("submit", function(event) {
+   event.preventDefault();
     let serverName = document.getElementById("server-select").value;
     let summonerName =  document.getElementById("summoner-name").value;
     if (!serverName || !summonerName) {
         return;
     }
     location.href = "/summoner/" + serverName + "/" + summonerName;
-}
+});
